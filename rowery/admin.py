3074780837174eece40b628410c01d4542be46cd
patyk2000 <101869338+patyk2000@users.noreply.github.com>
2022-05-20 15:29:27 +0200

    Zrobione działające models.py
    Wszystko git

--- rowery/admin.py
+++ rowery/admin.py
@@ -1,9 +1,10 @@
 from django.contrib import admin
 
-from .models import Producent, types, Osprzęt
+from .models import Producent, types, Rower, Wheel
 
 
 # Register your models here.
 admin.site.register(Producent)
 admin.site.register(types)
-admin.site.register(Osprzęt)
+admin.site.register(Rower)
+admin.site.register(Wheel)
--- rowery/models.py
+++ rowery/models.py
@@ -1,5 +1,10 @@
-
+from operator import truediv
+from turtle import ondrag
 from django.db import models
+from datetime import datetime
+from django.forms import CharField
+from tkinter import CASCADE
+from telnetlib import NOP
 
 class types(models.Model):
     name=models.CharField(max_length=200, null=True)
@@ -8,17 +13,24 @@ class types(models.Model):
         return self.name
 
 class Producent(models.Model):
-    producent=models.CharField(max_length=200)
-    model=models.ForeignKey(types, on_delete=models.CASCADE, null=True)
-
+    producent=models.CharField(max_length=200, null=True)
+   
     def __str__(self):
         return self.producent
 
+class Wheel(models.Model):
+    Wheel=models.CharField(max_length=200, null=True)
+    
 
-class Osprzęt(models.Model):
-    frame=models.CharField(max_length=200, null=True)
-    wheel=models.CharField(max_length=200, null=True)
-    forks=models.CharField(max_length=200, null=True)
-        
     def __str__(self):
-        return self.frame
+        return self.Wheel
+
+class Rower(models.Model):
+    frame=models.CharField(max_length=200, null=True)
+    types=models.ForeignKey(types, on_delete=models.CASCADE)
+    Wheel=models.ForeignKey(Wheel, on_delete=models.CASCADE)
+    forks=models.CharField(max_length=200, null=True)
+    Producent=models.ForeignKey(Producent, on_delete=models.CASCADE)
+    Rower=models.CharField(max_length=200, null=True)
+    def __str__(self):
+        return self.Rower
